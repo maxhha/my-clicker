eb99401b098f57d039fd15177870bea87aafa2c2
maxhha <devmaxhha@gmail.com>
2024-05-06 22:05:11 +0300

    Add checks on link creation

--- go-clicker/internal/storages/inmemory-link.go
+++ go-clicker/internal/storages/inmemory-link.go
@@ -1,6 +1,7 @@
 package storages
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 
@@ -8,35 +9,63 @@ import (
 	"github.com/teris-io/shortid"
 )
 
-// Not thread safe
+// Only for simple tests
 type InMemoryLinkStorage struct {
-	links_mu    sync.RWMutex
-	links       map[string]string
-	counters_mu sync.Mutex
-	counters    map[string]uint64
+	createTries   int
+	capacityLimit int
+	linksMx       sync.RWMutex
+	links         map[string]string
+	countersMx    sync.Mutex
+	counters      map[string]uint64
 }
 
 func NewInMemoryLinkStorage() InMemoryLinkStorage {
 	return InMemoryLinkStorage{
-		links_mu:    sync.RWMutex{},
-		links:       make(map[string]string),
-		counters_mu: sync.Mutex{},
-		counters:    make(map[string]uint64),
+		createTries:   10,
+		capacityLimit: 10,
+		linksMx:       sync.RWMutex{},
+		links:         make(map[string]string),
+		countersMx:    sync.Mutex{},
+		counters:      make(map[string]uint64),
 	}
 }
 
+var ErrFailGenerate = errors.New("fail generate unique short id")
+var ErrFull = errors.New("storage is full")
+
 // Create implements ports.LinkStorage.
 func (s *InMemoryLinkStorage) Create(redirect string) (string, error) {
-	id, err := shortid.Generate()
-	if err != nil {
-		return "", fmt.Errorf("shortid generate: %w", err)
+	var (
+		id  string
+		err error
+	)
+
+	s.linksMx.Lock()
+	defer s.linksMx.Unlock()
+
+	if len(s.links) > s.capacityLimit {
+		return "", ErrFull
 	}
 
-	s.links_mu.Lock()
-	defer s.links_mu.Unlock()
+	for try := 0; len(id) == 0 && try < s.createTries; try++ {
+		id, err = shortid.Generate()
+		if err != nil {
+			return "", fmt.Errorf("shortid.Generate: %w", err)
+		}
+
+		_, exists := s.links[id]
+		if exists {
+			id = ""
+		}
+	}
+
+	if len(id) == 0 {
+		return "", ErrFailGenerate
+	}
+
 	s.links[id] = redirect
-	s.counters_mu.Lock()
-	defer s.counters_mu.Unlock()
+	s.countersMx.Lock()
+	defer s.countersMx.Unlock()
 	s.counters[id] = 0
 
 	return id, nil
@@ -44,8 +73,8 @@ func (s *InMemoryLinkStorage) Create(redirect string) (string, error) {
 
 // GetCounter implements ports.LinkStorage.
 func (s *InMemoryLinkStorage) GetCounter(link string) (uint64, error) {
-	s.counters_mu.Lock()
-	defer s.counters_mu.Unlock()
+	s.countersMx.Lock()
+	defer s.countersMx.Unlock()
 	counter, ok := s.counters[link]
 	if ok {
 		return counter, nil
@@ -56,8 +85,8 @@ func (s *InMemoryLinkStorage) GetCounter(link string) (uint64, error) {
 
 // GetRedirect implements ports.LinkStorage.
 func (s *InMemoryLinkStorage) GetRedirect(link string) (string, error) {
-	s.links_mu.RLock()
-	defer s.links_mu.RUnlock()
+	s.linksMx.RLock()
+	defer s.linksMx.RUnlock()
 	redirect, ok := s.links[link]
 	if ok {
 		return redirect, nil
@@ -68,8 +97,8 @@ func (s *InMemoryLinkStorage) GetRedirect(link string) (string, error) {
 
 // IncrementCounter implements ports.LinkStorage.
 func (s *InMemoryLinkStorage) IncrementCounter(link string) error {
-	s.counters_mu.Lock()
-	defer s.counters_mu.Unlock()
+	s.countersMx.Lock()
+	defer s.countersMx.Unlock()
 	counter, ok := s.counters[link]
 	if ok {
 		s.counters[link] = counter + 1
